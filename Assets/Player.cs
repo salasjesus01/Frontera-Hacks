2dc72df21f9a4b4d646a47f67f0e16dfbc2d1d0a
salasjesus01 <salas.a.jesus01@gmail.com>
2023-02-11 17:47:47 -0600

    Player

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Player : MonoBehaviour
+{
+
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+
+        boxCollider = GetComponent<boxCollider2D>();
+
+    }
+
+}
